e7f1a136898d9badcffd2f4f2dc4ea90a517e60a
paritrs <parivallal.j@tworoadssoftware.com>
2015-03-19 12:09:28 +0000

    3 make files commit

---
+++ MakefileMethod1/mainfunc.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "func.h"
+int length(char*);
+
+using namespace std;
+
+int main()
+{
+ char str1[50];
+ int len;
+ cout<<"\n Enter the String: "<<endl;
+ cin>>str1;
+ len=length(str1);
+ cout<<"\n Length of the string = "<<len<<endl;
+ return 0;
+}
+
---
+++ MakefileMethod2/func.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "func.h"
+
+using namespace std;
+
+int length(char *s1)
+{
+ int l=0;
+ while(*s1!='\0')
+ {
+  l++;
+  s1++;
+ }
+ return l;
+}
